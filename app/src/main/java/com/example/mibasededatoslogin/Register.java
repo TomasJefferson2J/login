efca1518a47607192f6f78eb364d8632ccf23c36
TomasJefferson2J <1303874@senati.pe>
2023-03-23 20:04:23 -0500

    actualizacion

--- app/src/main/java/com/example/mibasededatoslogin/Register.java
+++ app/src/main/java/com/example/mibasededatoslogin/Register.java
@@ -3,6 +3,7 @@ package com.example.mibasededatoslogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -18,6 +19,15 @@ public class Register extends AppCompatActivity {
         editTextEmail = findViewById(R.id.email);
         editTextPassword = findViewById(R.id.password);
         buttonReg = findViewById(R.id.btn_register);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText().toString();
+            }
+        });
 
     }
 }
