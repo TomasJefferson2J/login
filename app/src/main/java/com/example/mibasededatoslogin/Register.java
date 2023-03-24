de01450cf8d6d6a78428b5593968b596bf28ffd5
TomasJefferson2J <1303874@senati.pe>
2023-03-23 19:54:46 -0500

    button

--- app/src/main/java/com/example/mibasededatoslogin/Register.java
+++ app/src/main/java/com/example/mibasededatoslogin/Register.java
@@ -3,12 +3,21 @@ package com.example.mibasededatoslogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Register extends AppCompatActivity {
 
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonReg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
+        
     }
 }
